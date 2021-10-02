ec4ee878bd74c8f2ddceaf54b9e5c39fe2192a1c
Augusto Sousa <lagrange.tesla@gmail.com>
2021-10-01 20:56:05 -0500

    Added two ways for calling the chuck norris api and handle the data

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const jokeUrl = 'https://api.chucknorris.io/jokes/random'
+
+
+/* With promises
+fetch( jokeUrl ).then( resp => {
+  resp.json().then( ({ id, value }) => {
+    console.log(id)
+    console.log(value)
+  })
+  
+}) */
+
+/* Chaining promises 
+fetch( jokeUrl )
+  .then( resp => resp.json() )
+  .then( ({ id, value }) => {
+    console.log(id)
+    console.log(value)
+  })
+*/
